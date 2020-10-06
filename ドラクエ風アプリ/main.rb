69881422eacf50b7f5a229d6aef88a36b2e83254
Lina Nagata <mad.20th@gmail.com>
2020-10-06 14:48:35 +0900

    update main.rb

--- ドラクエ風アプリ/main.rb
+++ ドラクエ風アプリ/main.rb
@@ -1,7 +1,5 @@
 class Brave
-  
   attr_reader :name, :hp, :offense, :defense
-
   attr_accessor :hp
 
   def initialize(**params)
@@ -12,14 +10,30 @@ class Brave
   end
 end
 
+class Monster
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+end
+
+
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+monster = monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 puts <<~TEXT
 NAME：#{brave.name}
 HP：#{brave.hp}
 OFFENSE：#{brave.offense}
 DEFENSE：#{brave.defense}
-TEXT
 
-brave.hp -= 30
-puts " #{brave.hp} "
+NAME：#{monster.name}
+HP：#{monster.hp}
+OFFENSE：#{monster.offense}
+DEFENSE：#{monster.defense}
+TEXT
