55b81334ef9b6487e0fe88d173498ddb54c5a854
Lina Nagata <mad.20th@gmail.com>
2020-10-06 13:17:30 +0900

    create main.rb

---
+++ ドラクエ風アプリ/main.rb
@@ -0,0 +1,38 @@
+class Brave
+  def initialize
+    @name = "テリー" 
+    @hp = 500
+    @offense = 150
+    @defense = 100
+
+  end
+
+  #nameのゲッター
+  def name
+    @name
+  end
+
+  #hpのゲッター
+  def hp
+    @hp
+  end
+
+  #offenseのゲッター
+  def offense
+    @offense
+  end
+
+  #defenseのゲッター
+  def defense
+    @defense
+  end
+end
+
+brave = Brave.new
+
+puts <<~TEXT
+NAME：#{brave.name}
+HP：#{brave.hp}
+OFFENSE：#{brave.offense}
+DEFENSE：#{brave.defense}
+TEXT
